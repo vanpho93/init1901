5c155e3f677b0e5a20a8678a849fd2c9adcf3820
Van Pho Nguyen <vanpho02@gmail.com>
2018-01-19 18:43:35 +0700

    init

---
+++ a.js
@@ -0,0 +1,7 @@
+var a = 1;
+let b = 2;
+const c = 3;
+// d = 10;
+console.log(c);
+// c = 10;
+console.log(a, b, c);
---
+++ b.js
@@ -0,0 +1,13 @@
+/*
+    static types
+    dynamic types
+*/
+let a = '1';
+/*
+    Nguyen thuy: string, number, boolean, undefined, null
+    Object: objects - functions - arrays
+*/
+
+a = 3;
+
+console.log(a);
